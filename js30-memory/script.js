2b7acbeaa32cc2621b95f030da11a5792eabccde
Viktor Ermakov <79091602201@yandex.ru>
2024-10-05 11:33:36 +0300

    feat: add lock board

--- js30-memory/script.js
+++ js30-memory/script.js
@@ -2,8 +2,12 @@ const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
 let firstCard, secondCard;
+let lockBoard = false;
 
 function flipCard() {
+    
+    if (lockBoard) return;
+    if (this === firstCard) return;
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
@@ -13,26 +17,34 @@ function flipCard() {
     }
 
     secondCard = this;
-    hasFlippedCard = false;
 
     checkForMatch();
 }
 
 function checkForMatch() {
-    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    let isMatch = firstCard.id === secondCard.id;
     isMatch ? disableCards() : unflipCards();
 }
 
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+    resetBoard();
 }
 
 function unflipCards() {
+
+    lockBoard = true;
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+        resetBoard();
     }, 1500);
 }
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
