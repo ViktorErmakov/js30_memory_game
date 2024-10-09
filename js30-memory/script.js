adfc904c4be7cc6eb992535c411a9d6838bef5fb
ViktorErmakov <79091602201@yandex.ru>
2024-10-09 23:16:16 +0300

    feat: finish task

--- js30-memory/script.js
+++ js30-memory/script.js
@@ -56,51 +56,52 @@ function resetBoard() {
     let flips = document.querySelectorAll('.flip');
     if (flips.length === cards.length) {
 
+        recordTableElement.innerHTML = '';
+        if (recordTable.length = 10) {
+            recordTable.splice(0, 1);
+        }
         recordTable.push(turns);
-        recordTable.sort();
         localStorage.setItem('recordTable', JSON.stringify(recordTable));
-        console.log(recordTable);
-
+        
         textContent = `Number of turns: \n`;
         recordTable.forEach(element => {
-            
+
         });
         for (let index = 0; index < recordTable.length; index++) {
-            
+
             const element = recordTable[index];
-            
+
             newString = document.createElement('div');
             newString.textContent = `${index + 1}. ${element}`;
-            if (element === turns) {
+
+            if (index === recordTable.length - 1 ) {
                 newString.style.fontWeight = 'bold';
                 newString.style.color = 'aqua';
-            }
+            };
+
             recordTableElement.appendChild(newString);
-            // найти такие же очки, если есть то только выделить жирным, если нет то добавить и выделить жирным.
-            // сделать ограничение только до 10 элементов
+
         }
-        
-        
+
         turns = 0;
         score.classList.add('score_active');
     }
 }
 
 function shuffle() {
-    // cards.forEach(card => {
-    //     let ramdomPos = Math.floor(Math.random() * 12);
-    //     card.style.order = ramdomPos;
-    // });
+    cards.forEach(card => {
+        let ramdomPos = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPos;
+    });
 };
 
 function AddFlipCard() {
-    
+
     cards.forEach(card => card.addEventListener('click', flipCard));
 
     newGame.addEventListener('click', () => {
-        
+
         turns = 0;
-        recordTableElement.innerHTML = '';
         let flips = document.querySelectorAll('.flip');
         flips.forEach(element => {
             element.classList.remove('flip');
@@ -108,11 +109,9 @@ function AddFlipCard() {
         score.classList.remove('score_active');
         AddFlipCard();
         shuffle();
-        
+
     });
 };
-
-
 
 AddFlipCard();
 shuffle();
